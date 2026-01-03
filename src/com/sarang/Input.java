1f26a52b320d52e06aa7ca64aecbbad7d6652dc9
Sarang Yadav <sarangdeshwal@gmail.com>
2026-01-03 11:40:31 +0530

    Initial Commit

---
+++ src/com/sarang/Input.java
@@ -0,0 +1,12 @@
+package com.sarang;
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+//        System.out.println(inp.nextInt());
+        int roll = inp.nextInt();
+        System.out.println("Your roll no is: " +  roll);
+
+    }
+}
---
+++ src/com/sarang/Primitives.java
@@ -0,0 +1,14 @@
+package com.sarang;
+
+public class Primitives {
+    public static void main(String[] args){
+
+        int roll = 64;
+        char letter = 'r';
+        float marks = 98.76f;
+        double large_decimal_no = 3459743.23422;
+        long large_integer_values = 234802347502375L;
+        boolean check = true;
+
+    }
+}
